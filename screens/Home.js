9f1da7023eb99813511e70b86172e6162a1a33f4
Jack Tiong <tiongyingkiat@gmail.com>
2021-04-13 01:57:05 +0800

    - Initial commit
    Add bottton tab bar
    
    Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>

---
+++ screens/Home.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import {
+    View,
+    Text,
+    TouchableOpacity,
+    StyleSheet
+} from 'react-native';
+
+const Home = ({ navigation }) => {
+    return (
+        <View style={styles.container}>
+        
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+    }
+})
+
+export default Home;
