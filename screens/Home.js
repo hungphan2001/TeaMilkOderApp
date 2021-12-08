e3abd2d04d4402046dfd743f7531c5469ecfa697
Hưng Phan <phandinhhungvp2001@gmail.com>
2021-12-08 20:08:48 +0700

    show/add vouncher in HomeScreen
    Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>

--- screens/Home.js
+++ screens/Home.js
@@ -13,7 +13,76 @@ import {connect} from 'react-redux';
 
 function renderAvailableRewards() {
   return (
-    <View></View>
+    <TouchableOpacity
+      style={{
+        flexDirection: 'row',
+        marginHorizontal: SIZES.padding,
+        marginTop: SIZES.padding,
+        height: 100,
+      }}
+      // onPress={() => navigation.navigate('Rewards')}
+    >
+      {/* Reward Cup */}
+      <View
+        style={{
+          width: 100,
+          height: '100%',
+          borderTopLeftRadius: 15,
+          borderBottomLeftRadius: 15,
+          backgroundColor: COLORS.pink,
+          alignItems: 'center',
+          justifyContent: 'center',
+        }}>
+        <ImageBackground
+          source={icons.reward_cup}
+          resizeMode="contain"
+          style={{
+            width: 85,
+            height: 85,
+            marginLeft: 3,
+            alignItems: 'center',
+            justifyContent: 'center',
+          }}>
+          <View
+            style={{
+              alignItems: 'center',
+              justifyContent: 'center',
+              width: 30,
+              height: 30,
+            }}>
+            <Text style={{color: COLORS.white, ...FONTS.h4}}>290</Text>
+          </View>
+        </ImageBackground>
+      </View>
+     {/* Reward Details */}
+      <View
+        style={{
+          flex: 1,
+          alignItems: 'center',
+          justifyContent: 'center',
+          backgroundColor: COLORS.lightPink,
+          borderRadius: 15,
+          marginLeft: -10,
+        }}>
+        <View>
+          <Text style={{color: COLORS.primary, ...FONTS.h2,
+            fontSize:20}}>
+            Avaiable Rewards</Text>
+
+          <View
+          style={{
+            marginTop:5,
+            padding:SIZES.base,
+            borderRadius:SIZES.radius *2,
+            backgroundColor: COLORS.primary
+          }}
+          >
+         <Text style={{color:COLORS.white,...FONTS.body3}}>150 points -$2.50 off</Text>
+          </View>
+        </View>
+      </View>
+      
+    </TouchableOpacity>
   );
 }
 
