f7389224f4aa8ffe162aa9fda9183ed1c427d24f
Hưng Phan <phandinhhungvp2001@gmail.com>
2021-12-09 15:15:07 +0700

    Order: Add header bar

--- screens/Order.js
+++ screens/Order.js
@@ -10,27 +10,42 @@ import {
     StyleSheet
 } from 'react-native';
 
+import Svg, { Circle } from 'react-native-svg'
+
 import { connect } from 'react-redux'
-import { TabButton, IconButton } from '../components'
+import { TabButton, IconButton, VerticalTextButton } from '../components'
 import { dummyData, COLORS, FONTS, SIZES, icons } from '../constants'
-const Order = ({ navigation,route,appTheme }) => {
 
-    const [selectedLocation, setSelectedLocation] =React.useState(null)
+const Order = ({ navigation, route, appTheme }) => {
 
-    React.useEffect(()=>{
-        let {selectedLocation } = route.params
-        setSelectedLocation(selectedLocation)
+    const [selectedLocation, setSelecytedLocation] = React.useState(null)
+    const [selectedTab, setSelectedTab] = React.useState(0)
+    const [selectedCategory, setSelectedCategory] = React.useState('Milk Tea')
+    const [menu, setMenu] = React.useState(null)
+
+    React.useEffect(() => {
+        let { selectedLocation } = route.params
+        setSelecytedLocation(selectedLocation)
     })
 
-    function renderHeaderSection(){
-        <SafeAreaView style={{
-            height: 200,
-            paddingTop: 10,
-            backgroundColor: COLORS.primary,
-            alignItems: 'center'
-        }}>
-        {/* Nav Bar  */}
-        <View
+    React.useEffect(() => {
+        const menuList = dummyData.menuList.filter(menuItem => menuItem.category === selectedCategory)
+        setMenu(menuList)
+    }, [selectedCategory])
+
+    const renderHeaderSection = () => {
+        return (
+            <SafeAreaView
+                style={{
+                    height: 160,
+                    paddingTop: 10,
+                    backgroundColor: COLORS.primary,
+                    alignItems: 'center'
+                }}>
+
+
+                {/* Nav Bar  */}
+                <View
                     style={{
                         flexDirection: 'row',
                         paddingHorizontal: SIZES.radius,
@@ -52,22 +67,36 @@ const Order = ({ navigation,route,appTheme }) => {
                     <View style={{ width: 25 }} />
                 </View>
                 {/* Location  */}
-                <View
-                    style={{
-                        marginTop: SIZES.radius,
-                        backgroundColor: COLORS.white1,
-                        paddingHorizontal: SIZES.radius,
-                        paddingVertical: 5,
-                        borderRadius: SIZES.padding
-                    }}
-                >
-                    <Text style={{ color: COLORS.primary, ...FONTS.body3 }}> {selectedLocation?.title} </Text>
-                </View>
-        </SafeAreaView>
+
+
+            </SafeAreaView>
+        )
     }
+
     return (
         <View style={styles.container}>
+            {/* Header  */}
             {renderHeaderSection()}
+            {/* Detail  */}
+            <View
+                style={{
+                    flex: 1,
+                }}
+            >
+                {/* Tab Bar  */}
+
+                {/* Side Bar & Listing  */}
+                <View
+                    style={{
+                        flex: 1,
+                        flexDirection: 'row',
+                    }}
+                >
+                    {/* Side Bar  */}
+
+                    {/* Listing  */}
+                </View>
+            </View>
         </View>
     )
 }
@@ -77,6 +106,7 @@ const styles = StyleSheet.create({
         flex: 1,
     }
 })
+
 
 function mapStateToProps(state) {
     return {
