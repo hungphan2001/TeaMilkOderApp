2d02f1d72fe76da88d99ee84c018bc078fcf7344
Hưng Phan <phandinhhungvp2001@gmail.com>
2021-12-09 14:43:55 +0700

    Location : Add overlay for location order
    Name & Bookmark
    Operation Hours
    Address
    Services
    Pick up
    Delivery

--- screens/Location.js
+++ screens/Location.js
@@ -57,7 +57,7 @@ const Location = ({ navigation, appTheme }) => {
     }
 
     const renderTopBarSection = () => {
-        return(
+        return (
             <View
                 style={{
                     flexDirection: 'row'
@@ -87,43 +87,162 @@ const Location = ({ navigation, appTheme }) => {
         )
     }
 
-    const renderLocationList = () => {
-       
+    const renderSearchBar = () => {
+        return (
+            <View
+                style={{
+                    flexDirection: 'row',
+                    marginTop: SIZES.radius,
+                    height: 50,
+                    paddingHorizontal: SIZES.padding,
+                    borderRadius: 25,
+                    backgroundColor: COLORS.lightGreen2,
+                    alignItems: 'center'
+                }}
+            >
+                <TextInput
+                    style={{
+                        flex: 1,
+                        height: 50,
+                        color: COLORS.black,
+                        ...FONTS.body3
+                    }}
+                    placeholder="enter your city, state or zip code"
+                    placeholderTextColor={COLORS.lightGray2}
+                />
+                <Image
+                    source={icons.search}
+                    style={{
+                        width: 30,
+                        height: 30,
+                        tintColor: COLORS.lightGray2
+                    }}
+                />
+            </View>
+        )
     }
 
-    const renderSearchBar = () => {
-        return(
-            <View
-            style={{
-                flexDirection:'row',
-                marginTop:SIZES.radius,
-                height: 50,
-                paddingHorizontal: SIZES.padding,
-                borderRadius: 25,
-                backgroundColor: COLORS.lightGreen2,
-                alignItems: 'center'
-            }}>
-                <TextInput
+    const renderLocationList = () => {
+        return (
+            <FlatList
                 style={{
-                    flex: 1,
-                    height: 50,
-                    color:COLORS.black,
-                    ...FONTS.body3
+                    marginTop: SIZES.radius,
+                    paddingHorizontal: SIZES.radius
                 }}
-                placeholder="enter your city, sate or zip code"
-                placeholderTextColor={COLORS.lightGray2}
-                >
-                
-                </TextInput>
-                <Image
-                source={icons.search}
-                style={{
-                    width: 30,
-                    height: 30,
-                    tintColor: COLORS.lightGray2,
-                }}
-        ></Image>
-            </View>
+                data={dummyData.locations}
+                keyExtractor={item => item.id}
+                showsVerticalScrollIndicator={false}
+                keyboardDismissMode='on-drag'
+                renderItem={({ item }) => (
+                    <TouchableOpacity
+                        style={{
+                            marginBottom: SIZES.radius,
+                            borderRadius: SIZES.radius * 2,
+                            paddingHorizontal: SIZES.padding,
+                            paddingVertical: SIZES.radius,
+                            backgroundColor: appTheme.cardBackgroundColor
+                        }}
+                        onPress={() => navigation.navigate('Order', {
+                            selectedLocation: item
+                        })}
+                    >
+                        {/* Name & Bookmark  */}
+                        <View
+                            style={{
+                                flexDirection: 'row'
+                            }}
+                        >
+                            <Text
+                                style={{ flex: 1, color: appTheme.textColor, ...FONTS.h2 }}
+                            >{item.title}</Text>
+                            <Image
+                                source={item.bookmarked
+                                    ? icons.bookmarkFilled
+                                    : icons.bookmark}
+                                style={{
+                                    height: 20,
+                                    width: 20,
+                                    tintColor: item.bookmarked
+                                        ? COLORS.red2
+                                        : COLORS.white
+                                }}
+                            />
+                        </View>
+                        {/* Address  */}
+                        <View
+                            style={{
+                                marginTop: SIZES.base,
+                                width: '80%'
+                            }}
+                        >
+                            <Text
+                                style={{
+                                    color: appTheme.textColor,
+                                    ...FONTS.body3,
+                                    lineHeight: 21
+                                }}
+                            >
+                                {item.address}</Text>
+                        </View>
+                        {/* Operation Hours  */}
+                        <View
+                            style={{
+                                marginTop: SIZES.base,
+                                marginBottom: SIZES.base,
+                            }}
+                        >
+                            <Text
+                                style={{
+                                    color: appTheme.textColor,
+                                    ...FONTS.body5,
+                                    lineHeight: 16
+                                }}
+                            >
+                                {item.operation_hours}</Text>
+                        </View>
+                        {/* Services */}
+                        <View
+                            style={{
+                                flexDirection: 'row',
+                                marginTop: SIZES.base,
+                            }}
+                        >
+                            {/* Pick up  */}
+                            <View
+                                style={{
+                                    borderColor: appTheme.textColor,
+                                    borderWidth: 1,
+                                    borderRadius: 20,
+                                    paddingHorizontal: SIZES.radius,
+                                    paddingVertical: 5
+                                }}
+                            >
+                                <Text style={{
+                                    color: appTheme.textColor,
+                                    ...FONTS.body3,
+                                }}>Pick-Up</Text>
+                            </View>
+                            {/* Delivery  */}
+                            <View
+                                style={{
+                                    borderColor: appTheme.textColor,
+                                    borderWidth: 1,
+                                    borderRadius: 20,
+                                    paddingHorizontal: SIZES.radius,
+                                    paddingVertical: 5,
+                                    marginLeft: 10
+                                }}
+                            >
+                                <Text style={{
+                                    color: appTheme.textColor,
+                                    ...FONTS.body3,
+                                }}>Delivery</Text>
+                            </View>
+                        </View>
+                    </TouchableOpacity>
+                )}
+
+            />
         )
     }
 
@@ -158,12 +277,7 @@ const styles = StyleSheet.create({
 function mapStateToProps(state) {
     return {
         appTheme: state.appTheme,
-        error: state.error
     }
 }
 
-function mapDispatchToProps(dispatch) {
-    return {}
-}
-
-export default connect(mapStateToProps, mapDispatchToProps)(Location)
+export default connect(mapStateToProps)(Location)
